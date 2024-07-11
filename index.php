e00a35e22d1a0af24eb0008853fe948cc4994705
Leandro Chaparro <leandrochaparro19@gmail.com>
2024-07-11 00:27:24 -0300

    Guia practica 1 php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+//guia practica-1  php
+
+// 1- Realice un script PHP que permita mostrar las características de interprete PHP
+// utilizando la función phpinfo().
+
+echo "1- <br>";
+phpinfo();
+
+// 2) - Realice un script que permita mostrar en pantalla el siguiente texto: Bienvenido al
+// lenguaje PHP!!!. Recuerde utilizar etiquetas estándares y la construcción echo.
+echo "2- <br>";
+echo "Bienvenido al lenguaje PHP!!!<br>";
+
+// 3) - Edite el script anterior y agregue una nueva sentencia que muestre el siguiente texto:
+// PHP es un lenguaje de programación interpretado que se ejecuta del lado del servidor. Pero ahora
+// utilice la función print() para mostrar el texto.
+
+echo "3- <br>";
+print("PHP es un lenguaje de programación interpretado que se ejecuta del lado del servidor <br>");
+
+echo "4- <br>";
+
+
+?>
